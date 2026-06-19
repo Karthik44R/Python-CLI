9f1b47be1977bb98e6c7fd46c4623457937d98c3
Karthikeya Reddy <karthikeyareddy1908@gmail.com>
2026-06-19 21:38:09 +0530

    Update menu

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -1,12 +1,12 @@
 menu = {
-    "espresso": {
+    "Espresso": {
         "ingredients": {
             "water": 300,
             "coffee": 20,
         },
         "cost": 100
     },
-    "latte": {
+    "Latte": {
         "ingredients": {
             "water": 230,
             "milk": 120,
@@ -14,7 +14,7 @@ menu = {
         },
         "cost": 150
     },
-    "cappuccino": {
+    "Cappuccino": {
         "ingredients": {
             "water": 200,
             "milk": 150,
@@ -41,11 +41,12 @@ def check_resources(order_ingredients):
 
 # Money Insertion & calculation
 def process_coins():
-    print("Please insert Coins: ")
+    print("Please insert Coins/Notes: ")
     coins_5 = int(input("how many ₹5 coin: "))
     coins_10 = int(input("how many ₹10 coin: "))
-    coins_20 = int(input("how many ₹20 coin: "))
-    total = coins_5 * 5 + coins_10 * 10 + coins_20 * 20
+    notes_10 = int(input("how many ₹10 notes: "))
+    notes_20 = int(input("how many ₹20 notes: "))
+    total = coins_5 * 5 + coins_10 * 10 + notes_10 * 10 + notes_20 * 20
     return total
 
 # Printing the BILL
@@ -64,12 +65,19 @@ def is_payment_successful(money_received, coffee_cost):
 def make_coffee(coffee_name, coffee_ingredient):
     for item in coffee_ingredient:
         resources[item] -= coffee_ingredient[item]
-    print(f"here is your {coffee_name} ☕.... enjoy")
+    print(f"here is your {coffee_name}☕.... Enjoy\nThankYou😊")
 
 # While Loop runs continuously until typing off
 is_on = True
 while is_on:
-    choice = input("what would you like to have (latte/espresso/cappuccino): ")
+    choice = input("What would you like to have \n1.Espresso\n2.Latte\n3.Cappuccino) : ").strip()
+    if choice == '1':
+        choice = 'Espresso'
+    elif choice == '2':
+        choice = 'Latte'
+    elif choice == '3':
+        choice = 'Cappuccino'
+
     if choice == 'off':
         is_on = False
     elif choice == 'on':
@@ -78,12 +86,13 @@ while is_on:
         print(f"water={resources['water']}ml")
         print(f"milk={resources['milk']}ml")
         print(f"coffee={resources['coffee']}g")
-        print(f"money=rupees{profit}")
-    else:
+        print(f"money=₹{profit}")
+    if choice in menu:
         coffee_type = menu[choice]
         if check_resources(coffee_type['ingredients']):
             payment = process_coins()
             if is_payment_successful(payment, coffee_type['cost']):
                 make_coffee(choice, coffee_type['ingredients'])
-
+    else:
+        print("Invalid Choice! Please choose 1 for Espresso, 2 for Latte, 3 for Cappuccino, 'report', or 'off'.\n")    
 
